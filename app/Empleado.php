1f729a6eed629ba8c877d1905ab40838d50802ff
mercydisatel <smercedes66@hotmail.com>
2019-03-07 13:18:51 -0600

    migracion a BD completa

--- app/Empleado.php
+++ app/Empleado.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Empleado extends Model
 {
     //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nombreEmpleado', 
+        'idDepartamento',
+    ];
+
+    public $timestamps = false;
+    protected $primaryKey = 'idEmpleado';
 }
--- app/Solicitud.php
+++ app/Solicitud.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Solicitud extends Model
 {
-    //
+     //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'comentarioSolicitud', 'estadoSolicitud', 
+        'horaInicio', 'horaFin', 'fechaActual',
+        'idDepartamento',
+    ];
+
+    public $timestamps = false;
+    protected $primaryKey = 'idSolicitud';
 }
