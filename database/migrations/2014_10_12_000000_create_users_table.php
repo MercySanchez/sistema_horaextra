c19eb920699c9fc1e7e22cfe7708521d61720b89
mercydisatel <smercedes66@hotmail.com>
2019-03-08 09:37:31 -0600

    cmabios en migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('rol');
-            $table->string('username')->unique();;
+            $table->string('username')->unique();
             $table->string('password');
             
             $table->rememberToken();
