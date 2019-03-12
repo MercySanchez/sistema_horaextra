0a18c806e44adbb7b185a55c0b1e2a6963f6d381
MercySanchez <mercedes.sanchez@grupodisatel.com>
2019-03-12 13:48:27 -0600

    integracion inicial de plantilla

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,65 +1,92 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">    
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-        <div class="panel panel-primary" >
-            <div class="panel-heading">
-                <div class="panel-title">Iniciar Sesión</div>
-            </div>     
 
-            <div style="padding-top:30px" class="panel-body" >
+<!doctype html>
+
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>DISATEL</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link href="fonts/circular-std/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="libs/css/style.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/fontawesome-all.css">
+    <style>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+    }
+    </style>
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- login page  -->
+    <!-- ============================================================== -->
+    <div class="splash-container">
+        <div class="card ">
+            <div class="card-header text-center"><a href="/login"><img class="logo-img" src="img/logo.png" width="100" height="70" alt="logo"></a></div>
+            <div class="card-body">
 
                 <!--Mensaje de error -->
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
 
-                <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                   <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">  <!--Seguridad Otorgada por blade -->
 
+                <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">  <!--Seguridad Otorgada por blade -->
 
                     <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                        <label for="username" class="col-md-4 control-label">Nombre de Empleado</label>
+                        
+                        <input class="form-control form-control-lg" id="username" type="text" name="username" placeholder="Usuario" value="{{ old('username') }}" required autofocus>
 
-                        <div class="col-md-6">
-                            <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
 
                             @if ($errors->has('username'))
                             <span class="help-block">
                                 <strong>{{ $errors->first('username') }}</strong>
                             </span>
                             @endif
-                        </div>
+
+
                     </div>
-
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password" class="col-md-4 control-label">Contraseña</label>
+                        <input class="form-control form-control-lg" id="password" type="password" placeholder="Contraseña"  name="password">
 
-                        <div class="col-md-6">
-                            <input id="password" type="password" class="form-control" name="password" required>
-
-                            @if ($errors->has('password'))
+                        @if ($errors->has('password'))
                             <span class="help-block">
                                 <strong>{{ $errors->first('password') }}</strong>
                             </span>
                             @endif
-                        </div>
-                    </div>
 
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-4">
-                        </div>
                     </div>
-
                     <div class="form-group">
-                        <div class="col-md-8 col-md-offset-4">
-                            <button type="submit" class="btn btn-primary">
-                                Iniciar Sesión
-                            </button>
-                        </div>
+                        
                     </div>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block">Iniciar Sesión</button>
                 </form>
             </div>
+            
         </div>
     </div>
+  
+    <!-- ============================================================== -->
+    <!-- end login page  -->
+    <!-- ============================================================== -->
+    <!-- Optional JavaScript -->
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+ 
+</html>
 
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/crearSolicitud', 'SolicitudController@create')->name('crearSolicitud');
+
